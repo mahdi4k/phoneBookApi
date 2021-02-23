e613a9a818417b2593a6fedd563f4d2b90a8c5b2
mahdi2k <falahati.mahdi@yahoo.com>
2021-02-23 13:41:41 +0330

    validating Audience added

--- app/Http/Controllers/Api/v1/AudienceController.php
+++ app/Http/Controllers/Api/v1/AudienceController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api\v1;
 use App\Audience;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class AudienceController extends Controller
 {
@@ -12,6 +13,22 @@ class AudienceController extends Controller
     {
         $audience = Audience::all();
         return response()->json($audience);
+    }
+
+    public function store(Request $request)
+    {
+        $Validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'phoneNumber' => 'required|regex:/(09)[0-9]{9}/|digits_between:10,11',
+            'email' => 'required|'
+        ]);
+
+        if ($Validator->fails()) {
+            return response([
+                'data' =>  $Validator->errors(),
+                'status' => 'error'
+            ], 422);
+        }
     }
 
 }
--- routes/api.php
+++ routes/api.php
@@ -16,4 +16,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1')->namespace('Api\v1')->group(function (){
    Route::get('/Audience','AudienceController@index');
+   Route::post('Audience','AudienceController@store');
 });
